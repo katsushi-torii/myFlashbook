ea27f596fda59c95b396d43c789832f0b402aa8a
Katsushi Torii <kat352soccer1@gmail.com>
2023-12-14 18:41:27 +0900

    html & css done for all pages

--- Components/js/form.js
+++ Components/js/form.js
@@ -5,10 +5,12 @@ for(let i = 1; i < 6; i++){
     `);
     $('#meaningNum').append(newOption);
 }
-//点数が選ばれるたびにその数にあった得点者記入欄を表示
+
+//the input will appear depending on which number is selected on the select
 $('#meaningNum').on('change', function(){
-    //得点者inputをリセット
+    //reset the input
     $('.meanings input').remove();
+    
     for(let i = 0; i < $('#meaningNum').val(); i++){
         let newInput = $(`     
             <input type="text" name="meaning${i}" id="meaning${i}">
